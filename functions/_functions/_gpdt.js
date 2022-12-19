fc2a46df5701f1de671544532379a6f75f06357a
Gabriel Henrique <83310622+G4br1e3l@users.noreply.github.com>
2022-12-18 22:17:10 -0300

    Delete _gpdt.js

--- functions/_functions/_gpdt.js
+++
@@ -1,7 +0,0 @@
-export const get_group_data = async (MP, msg) => {
-    let Gp = ``
-    setTimeout(async () => Gp = await MP.groupMetadata(msg?.messages[0]?.key?.remoteJid), 6000)
-    if(Gp === ``) Gp = await MP.groupMetadata(msg?.messages[0]?.key?.remoteJid ?? 'undefined')
-    setInterval(() => { Gp = `` }, 10_000)
-    return Gp
-}
